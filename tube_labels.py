faaeace6baf1bbca9923983d4b93839d9e6ffafc
Alan Rupp <alan.rupp@gmail.com>
2019-04-12 10:51:39 -0400

    added argument to make more than 1 sheet

--- tube_labels.py
+++ tube_labels.py
@@ -1,6 +1,6 @@
 #!/home/alanrupp/anaconda3/bin/python
 
-## Create script to generate tube label sheet (12x16 labels)
+## Create script to generate tube label sheet(s) (12x16 labels)
 import numpy as np
 import pandas as pd
 import xlsxwriter
@@ -9,53 +9,93 @@ import argparse
 
 # parse command line arguments
 parser = argparse.ArgumentParser(description="Generate a sheet of tube labels")
-parser.add_argument("--start", type=str)
-parser.add_argument("--size", type=str, default='1/2"')
-parser.add_argument("--outfile", type=str, default="tube_labels.xlsx")
+parser.add_argument('--start', help='specify a start value', type=str)
+parser.add_argument('--end', help='optional end value (default makes 1 sheet)',\
+                    type=str, default=False)
+parser.add_argument("--size", help='sticker size in inches (default is 1/2")', \
+                    type=str, default='1/2"')
+parser.add_argument("--outfile", help='name of output file (default is tube_labels.xlsx)', \
+                    type=str, default='tube_labels.xlsx')
 args = parser.parse_args()
 
-# grab start value and parse letter and number
-spaceOne = args.start
-letter = re.findall('[A-Za-z]+', spaceOne)[0]
-if re.search('[0-9]+', spaceOne):
-    number = int(re.findall('[0-9]+', spaceOne)[0])
-else:
-    number = list()
+# funciton to grab letter and number
+def parse_label(label):
+    letter = re.findall('^[A-Za-z]+', label)
+    if len(letter) < 1:
+        letter = ""
+    else:
+        letter = letter[0]
+    # grab number
+    if re.search('[0-9]+$', label):
+        number = int(re.findall('[0-9]+$', label)[0])
+    else:
+        number = ""
+    return(letter, number)
 
-# generate matrix of values
+# find first letter and/or number
+first_letter, first_number = parse_label(args.start)
+
+# find last letter and/or number
+if not args.end:
+    last_number = first_number + 191
+    last_letter = first_letter + str(last_number)
+else:
+    last_letter, last_number = parse_label(args.end)
+    if last_letter != first_letter:
+        print("\nError: Start and End values are not compatible")
+        exit()
+    else:
+        letter = first_letter
+    if type(last_number) != type(first_number):
+        print("\nError: Start and End values are not compatible")
+        exit()
+
+# generate all the values
 labels = list()
+number = first_number
 if isinstance(number, int):
-    while number <= 1000 and len(labels) < 192:
+    while number <= 1000 and number <= last_number:
         value = letter + str(number)
         number += 1
         labels.append(value)
 else:
     labels = [letter] * 192
 
-# put into pandas dataframe
-labels = np.array(labels).reshape(16,12)
+# - put into pandas dataframe  ------------------------------------------------
+total_labels = len(labels)
+if total_labels % (12*16) != 0:
+    while total_labels % (12*16) != 0:
+        labels.append('')
+        total_labels += 1
+rows = int(total_labels / 12)
+
+labels = np.array(labels).reshape(rows, 12)
 labels = pd.DataFrame(labels)
 
-# prepare Excel writer
+# - prepare Excel writer ------------------------------------------------------
+total_sheets = int(len(labels) / 16)
 writer = pd.ExcelWriter(path=args.outfile, engine='xlsxwriter')
 workbook = writer.book
 
 # adjust font size for each label
-labelSize = args.size
-if labelSize == '1/2"':
+if args.size == '1/2"':
     fontSize = 11
-elif labelSize == '3/8"':
+elif args.size == '3/8"':
     fontSize = 10
 
-cell_format = workbook.add_format({'text_wrap': True, 'align': 'center',
+# add formatting for appropriate font sizes and centering
+cell_format = workbook.add_format({'text_wrap': True, 'align': 'center',\
     'valign': 'vcenter', 'font_name': 'Arial', 'font_size': fontSize})
 
 # write to Excel
-labels.to_excel(writer, sheet_name='Sheet1', header=False, index=False)
-worksheet = writer.sheets['Sheet1']
-worksheet.set_column('A:L', 5.7, cell_format)
-for i in range(16):
-    worksheet.set_row(i, 45)
-worksheet.set_margins(left=0.5, right=0.5, top=0.5, bottom=0.5)
+for sheet in range(total_sheets):
+    labels_write = labels.iloc[sheet*16: (sheet+1)*16]
+    labels_write.to_excel(writer, sheet_name='Sheet' + str(sheet), \
+                     header=False, index=False)
+    worksheet = writer.sheets['Sheet' + str(sheet)]
+    worksheet.set_column('A:L', 5.7, cell_format)
+    for i in range(16):
+        worksheet.set_row(i, 45)
+    worksheet.set_margins(left=0.5, right=0.5, top=0.5, bottom=0.5)
 
 workbook.close()
